2ff7b485612511d5ed66329e475677b6994c3069
yanluxiaocen <formulatree@qq.com>
2026-07-19 17:58:40 +0800

    forth

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-    cout<<"come here"<<endl;
-	cout << "I found you" << endl;
+int main()
+{
+    cout << "come here" << endl;
+    cout << "I found you" << endl;
+    getchar();
+    int a = 10;
+    for (int i = 0; i < a; i++)
+    {
+        cout << "i: " << i;
+        cout << "well" << i << endl;
+    }
     getchar();
     return 0;
 }
