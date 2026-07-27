d352e65cbb069f4179e548a9d9a3efe07c79fa9d
演戮笑涔 <formulatree@qq.com>
2026-07-27 16:07:00 +0800

    End
    Removed the loop that prints 'i' and 'well'.

--- main.cpp
+++ main.cpp
@@ -8,13 +8,6 @@ int main()
     cout << "come here" << endl;
     cout << "I found you" << endl;
     getchar();
-    int a = 10;
-    for (int i = 0; i < a; i++)
-    {
-        cout << "i: " << i;
-        cout << "well" << i << endl;
-    }
-    getchar();
 
     return 0;
 }
